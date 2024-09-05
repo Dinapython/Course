d7cac4fe5dc18d4dfe768dfbb54fc1deae367b6a
Dinapython <gafurovamadi@gmail.com>
2024-09-05 23:22:14 +0300

    Add files via upload

---
+++ Peremennie.py
@@ -0,0 +1,6 @@
+number_of_completed_tasks = 12
+number_of_hours_spent = 1.5
+Course_name = 'Python,'
+print(number_of_hours_spent/number_of_completed_tasks)
+print('Курс:', Course_name,'всего задач:',number_of_completed_tasks,
+      'затрачено часов:',number_of_hours_spent, 'среднее время выполнения: 0.125 часа')
